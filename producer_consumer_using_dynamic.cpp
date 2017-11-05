b3bf29fcdcc1f4f9c974d74bfd5622e7e6f0e0ce
aravind-kumar <karavindkumar1993@gmail.com>
2017-11-04 23:43:22 -0400

    Producer consumer using json and generic consumer

--- producer_consumer_using_dynamic.cpp
+++ producer_consumer_using_dynamic.cpp
@@ -6,20 +6,51 @@
 #include <folly/ProducerConsumerQueue.h>
 
 template<typename T>
-auto getConsumer(std::function<void (T input)> givenConsumerFn)
+auto getConsumer(folly::ProducerConsumerQueue<T>& queue,
+	         std::function<void (T input)> givenConsumerFn)
 {
-   auto consumerFn = [givenConsumerFn](T givenInput)
+   auto consumerFn = [&queue,givenConsumerFn]
 		     {
-     			givenConsumerFn(givenInput);
+                        for(;;)
+                        {
+                          T input;
+                          while(!queue.read(input))
+				continue;
+     			  givenConsumerFn(input);
+                        }
                      };
    return consumerFn;
 
 }
 
-folly::ProducerConsumerQueue<folly::dynamic> queue(100);
+void produceJsonObjects(folly::ProducerConsumerQueue<folly::dynamic>& queue)
+{
+   std::string jsonDocument1 = R"({"aravind":122,"kumar":[false, null, true, "false"]})";
+   std::string jsonDocument2 = R"({"aravind":123,"kumar":[false, null, true, "false"]})";
+   int count=0;
+   for(;;)
+   {
+      ++count;
+      folly::dynamic d1 = folly::parseJson(jsonDocument1);
+      folly::dynamic d2 = folly::parseJson(jsonDocument2);
+      folly::dynamic final = count%2 ? d1 : d2;
+      while(!queue.write(final))
+         continue;
+   }
+}
+
 int main()
 {
+   folly::ProducerConsumerQueue<folly::dynamic> queue(100);
+   std::function<void(folly::dynamic)> 
+   printDynamicObject = [] (folly::dynamic input) 
+  	         	{ std::cout<<folly::toPrettyJson(input);};
 
+   auto consumerFn = getConsumer(queue,printDynamicObject); 
+    
+   std::thread consumer(consumerFn);
+   
+   produceJsonObjects(queue); 
    return 0;
 }
 
